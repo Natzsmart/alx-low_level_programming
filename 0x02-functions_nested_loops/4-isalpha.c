0534fff4d3d1fcffaedd4f0926e3f216d4e75d0e
Smartino <Iniobongeffiong40@gmail.com>
2022-06-07 13:26:25 -0700

    checks for alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isalpha - checks for alphabetic character
- * the character to be checked
+ * @c: the integer value it receives
  * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
