7a188f48f653d81cf4c0d99d27833a64be6951b6
Smartino <Iniobongeffiong40@gmail.com>
2022-06-09 07:56:11 -0700

    prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints 0 - 9 except 2, 3
- * Description: prints
+ * print_numbers - print  0 - 9
+ *
  * Return: void
  */
-void print_most_numbers(void)
+
+void print_numbers(void)
 {
-	int a = 0;
+	char a = 0;
 
 	while (a <= 9)
 	{
-		if (a != 2 && a != 4)
-		{
-			_putchar(a + '0');
-		}
+		_putchar(a + '0');
 		a++;
 	}
 	_putchar('\n');
