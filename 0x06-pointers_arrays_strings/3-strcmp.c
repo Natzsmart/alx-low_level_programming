a69275cfbdb63f2bc8e946054deb8830685e7bc1
Smartino <Iniobongeffiong40@gmail.com>
2022-06-15 22:47:37 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,7 +9,6 @@
  * 	   If str1 == str2, 0.
  * 	   If str1 > str2, the positive difference
  */
-
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && *s2 && *s1 == *s2)
@@ -17,6 +16,5 @@ int _strcmp(char *s1, char *s2)
 		s1++;
 		s2++;
 	}
-
 	return (*s1 - *s2);
 }
