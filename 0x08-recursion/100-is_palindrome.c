e96873c53eaf528587548c7450c2cb2d112c6a10
Smartino <Iniobongeffiong40@gmail.com>
2022-06-22 18:26:30 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -17,10 +17,10 @@ int find_strlen(char *s)
 	if (*(s + len))
 	{
 		len++;
-		len += find_strlen(s + len)
+		len += find_strlen(s + len);
 	}
 
-	return (len)
+	return (len);
 }
 
 /**
@@ -54,5 +54,10 @@ int check_palindrome(char *s, int len, int index)
 int is_palindrome(char *s)
 {
 	int index = 0;
+	int len = find_strlen(s);
 
+	if (!(*s))
+		return (1);
 
+	return (check_palindrome(s, len, index));
+}
