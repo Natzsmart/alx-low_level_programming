3f0f4ec23993c45ae0d5a648451f8d2f7944aa98
Smartino <Iniobongeffiong40@gmail.com>
2022-07-06 08:22:12 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,9 +3,8 @@
 /**
  * print_name - Prints a name.
  * @name: The name to be printed.
- * @f: A pointer to a function that prints a name
+ * @f: A pointer to a function that prints a name.
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
